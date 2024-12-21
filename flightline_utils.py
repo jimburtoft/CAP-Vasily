3333a6874f6e688f5652964fd14d8e550e42b1e9
Jim Burtoft <39492751+jimburtoft@users.noreply.github.com>
2024-12-21 14:56:30 -0500

    initial upload

---
+++ flightline_utils.py
@@ -0,0 +1,252 @@
+# flightline_utils.py
+import sys
+import os
+import math
+import numpy
+from bas_air_unit_network_dataset.exporters.fpl import (
+    Fpl,
+    Waypoint as FplWaypoint,
+    Route as FplRoute,
+    RoutePoint as FplRoutePoint,
+)
+#This is here to make sure the schema files get included in the pyinstaller build
+import bas_air_unit_network_dataset.schemas.garmin
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+# get the bearing from a pair of coordinates
+def get_bearing(lat1, long1, lat2, long2):
+    #code below taken from stack overflow https://stackoverflow.com/questions/54873868/python-calculate-bearing-between-two-lat-long
+    dLon = (long2 - long1)
+    x = math.cos(math.radians(lat2)) * math.sin(math.radians(dLon))
+    y = math.cos(math.radians(lat1)) * math.sin(math.radians(lat2)) - math.sin(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.cos(math.radians(dLon))
+    brng = numpy.arctan2(x,y)
+    brng = numpy.degrees(brng)
+    return brng
+#flip the rows as needed and return an object with the flipped rows
+def flip_rows(flightlines,choice, flip):
+    if choice == 1:
+        start = 0
+    elif choice == 2:
+        start = 1
+    elif choice == 3:
+        start = (len(flightlines) % 2) + 1
+    elif choice == 4:
+        start = len(flightlines) % 2
+    if flip == True:
+        for i, row in enumerate(flightlines):#This flips every other line, depending on whether we start with the first line or the second
+            if (i + start) % 2 == 1:
+                flightlines[i] = row[::-1]
+    if flip == False and choice == 2:
+        for i, row in enumerate(flightlines):
+            flightlines[i] = row[::-1] # if you choose 2, flip all the lines
+    if choice == 3 or choice == 4: #if you choose two points at the bottom, flip the whole matrix
+        flightlines = numpy.flipud(flightlines)
+        #todo:  If you implement choice 3/4, add something like this (I think it should work, but not tested):
+        #if flip == False and choice == 4:
+        #    for i, row in enumerate(flightlines):
+        #        flightlines[i] = row[::-1] # if you choose 2, flip all the lines
+    return flightlines
+
+#Given a point, bearing and distance, return the new point.  Code from Vasily
+def getEndpoint(lat1,lon1,bearing,d):
+    R = 6371                     #Radius of the Earth
+    brng = math.radians(bearing) #convert degrees to radians
+    d = d*1.852                  #convert nautical miles to km
+    lat1 = math.radians(lat1)    #Current lat point converted to radians
+    lon1 = math.radians(lon1)    #Current long point converted to radians
+    lat2 = math.asin( math.sin(lat1)*math.cos(d/R) + math.cos(lat1)*math.sin(d/R)*math.cos(brng))
+    lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
+    lat2 = math.degrees(lat2)
+    lon2 = math.degrees(lon2)
+    return lat2,lon2
+
+
+#Given a point, bearing and distance, return the new point.  Code from Vasily
+def getEndpoint(lat1,lon1,bearing,d):
+    R = 6371                     #Radius of the Earth
+    brng = math.radians(bearing) #convert degrees to radians
+    d = d*1.852                  #convert nautical miles to km
+    lat1 = math.radians(lat1)    #Current lat point converted to radians
+    lon1 = math.radians(lon1)    #Current long point converted to radians
+    lat2 = math.asin( math.sin(lat1)*math.cos(d/R) + math.cos(lat1)*math.sin(d/R)*math.cos(brng))
+    lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
+    lat2 = math.degrees(lat2)
+    lon2 = math.degrees(lon2)
+    return lat2,lon2
+
+def add_endpoints(flightlines):
+    """
+    Add endpoints to each flightline.
+    
+    Args:
+    flightlines (list): A list of flightlines, where each flightline is a list of coordinates.
+    
+    Returns:
+    list: A new list of flightlines with endpoints added.
+    """
+    flightlinesnew = []
+    for line in flightlines: #from point 1, get the bearing from point 0 to point 1 and add .5NM.  From point 0, get the bearing from point 1 to point 0 and add .5NM
+        point1 = getEndpoint(line[1][0],line[1][1],get_bearing(line[0][0],line[0][1],line[1][0],line[1][1]),0.5)
+        point2 = getEndpoint(line[0][0],line[0][1],get_bearing(line[1][0],line[1][1],line[0][0],line[0][1]),0.5)
+        line =  [point2,line[0],line[1],point1]
+        flightlinesnew.append(line)
+    return flightlinesnew
+
+def flip_rows(flightlines, choice, flip):
+    """
+    Flip rows of flightlines based on the given choice and flip parameter.
+    
+    Args:
+    flightlines (list): A list of flightlines.
+    choice (int): The starting corner choice (1 or 2).
+    flip (bool): Whether to use sawtooth pattern (True) or unidirectional (False).
+    
+    Returns:
+    list: A new list of flightlines with rows flipped according to the parameters.
+    """
+    if choice == 1:
+        start = 0
+    elif choice == 2:
+        start = 1
+    elif choice == 3:
+        start = (len(flightlines) % 2) + 1
+    elif choice == 4:
+        start = len(flightlines) % 2
+    if flip == True:
+        for i, row in enumerate(flightlines):#This flips every other line, depending on whether we start with the first line or the second
+            if (i + start) % 2 == 1:
+                flightlines[i] = row[::-1]
+    if choice == 3 or choice == 4: #if you choose two points at the bottom, flip the whole matrix
+        flightlines = numpy.flipud(flightlines)
+    return flightlines
+
+def add_keyholes(flightlines):
+    """
+    Add keyhole points to flightlines.
+    
+    Args:
+    flightlines (list): A list of flightlines.
+    
+    Returns:
+    list: A new list of flightlines with keyhole points added.
+    """
+    # Implement your keyhole adding logic here
+    # This is a placeholder implementation
+    flightlinesnew = []
+    for i,line in enumerate(flightlines):
+        #This was the logic Vasily came up with for turning right vs turning left.  
+        # We compare the bearing of the line to the bearing of the first point on the next line
+        if  i < len(flightlines)-1: #since we are looking ahead and we don't need keyholes on the final line
+            if abs(get_bearing(line[0][0],line[0][1],line[1][0],line[1][1])-get_bearing(line[-1][0],line[-1][1],flightlines[i+1][0][0],flightlines[i+1][0][1])) > 180:
+                bearing=get_bearing(line[0][0],line[0][1],line[1][0],line[1][1])-get_bearing(line[-1][0],line[-1][1],flightlines[i+1][0][0],flightlines[i+1][0][1]) +360
+            else:
+                bearing=get_bearing(line[0][0],line[0][1],line[1][0],line[1][1])-get_bearing(line[-1][0],line[-1][1],flightlines[i+1][0][0],flightlines[i+1][0][1])
+            if bearing > 0: #Go out 2NM at +/- 45 degrees depending on bearing
+                point1 = getEndpoint(line[-1][0],line[-1][1],get_bearing(line[0][0],line[0][1],line[1][0],line[1][1])+45,2)
+            else:
+                point1 = getEndpoint(line[-1][0],line[-1][1],get_bearing(line[0][0],line[0][1],line[1][0],line[1][1])-45,2)
+            line.append(point1)
+        if i>0: #if not the first line, add a keyhole point 3NM out at the beginning of the line
+            point1 = getEndpoint(line[0][0],line[0][1],get_bearing(line[1][0],line[1][1],line[0][0],line[0][1]),3)
+            line.insert(0,point1)
+
+                
+        flightlinesnew.append(line)
+    return flightlinesnew
+
+
+def output_points(output, flightlines):
+    """
+    Output the flightlines to the given output.
+    
+    Args:
+    output: Either a file path (str) or a file-like object.
+    flightlines (list): A list of flightlines to output.
+    """
+    # Implement your output logic here
+    # This function should be able to handle both file paths and file-like objects
+    if isinstance(output, str):
+        with open(output, 'w') as f:
+            for flightline in flightlines:
+                for coord in flightline:
+                    f.write(str(coord[0]) + "/" + str(coord[1]) + " ")
+    else:
+        for flightline in flightlines:
+            for coord in flightline:
+                output.write(str(coord[0]) + "/" + str(coord[1]) + " ")
+
+def fpl_export(flightlines, output_file, prefix):
+    """
+    Export flightlines to an FPL file.
+    
+    Args:
+    flightlines (list): A list of flightlines to export.
+    output_file (str): The path to the output file.
+    prefix (str): A three-character prefix for the FPL file.
+    """
+
+    path = output_file
+
+    #Build the Waypoints for the FPL file
+    waypoints = []
+    for lineno, line in enumerate(flightlines):
+        for coordno, coord in enumerate(line):
+            waypoint = FplWaypoint()
+            waypoint.latitude = coord[0]
+            waypoint.longitude = coord[1]
+            waypoint.comment = ""
+            waypoint.type = "USER WAYPOINT"
+            waypoint.identifier = prefix + "L" +str(lineno) + "P" + str(coordno)
+            waypoints.append(waypoint)
+
+    fpl = Fpl() #from the bas library
+    route = FplRoute()
+    route_name_string = os.path.basename(output_file).rstrip(".fpl")[:14] # grab the file name, remove the extension, and only use the first 14 characters
+    route.name = route_name_string # because we will update route.name if there are more than 94 points, and we want to keep the string around
+    print("route name", route.name)
+    route.index = 1
+
+    #Build the RoutePoints for the route part of the FPL file
+    for route_waypoint in waypoints:
+        route_point = FplRoutePoint()
+        route_point.waypoint_identifier = route_waypoint.identifier
+        route_point.waypoint_type = "USER WAYPOINT"
+        #route_point.waypoint_country_code = "__"
+        route.points.append(route_point)
+        # once you get above 94 points, add a "2" to the filename and the route name, dump it, reset the route, reset the name, and go back to accumulating.  
+        # this may be writing multiple times (I don't care) and this keeps all the waypoints in the file
+        # it also breaks if you have more than 200 points, but that is unlikely.
+        if len(route.points) > 94: 
+            path2=path.replace(".fpl", "2.fpl")
+            print ("\n FPL path has more than 100 points.  Multiple files being exported to:", path2)
+            fpl.waypoints = waypoints
+            fpl.route = route
+            route.name = route_name_string + "2"
+            fpl.dump_xml(path2)
+            route = FplRoute()
+            route.name = route_name_string
+            route.index = 1
+
+
+
+
+
+    if len(route.points) < 100:
+        #Save the FPL file to the path
+        print("\nFPL file saved to:", path)
+        #path=path.joinpath(file_name_with_date("00_WAYPOINTS_{{date}}.fpl"))
+        fpl.waypoints = waypoints
+        fpl.route = route
+        fpl.dump_xml(path)
+    else:
+        print ("\n\n*******************There are too many lines in your original KML file")   
---
+++ vgui.py
@@ -0,0 +1,291 @@
+import sys
+import os
+import xml.etree.ElementTree as ET
+import PyQt5
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QFileDialog, 
+                             QLineEdit, QMessageBox, QGroupBox, QRadioButton, QScrollArea, QCheckBox, QInputDialog)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+import webview
+import io
+
+# Import the utility functions
+from flightline_utils import add_endpoints, flip_rows, add_keyholes, output_points, fpl_export, resource_path
+
+#To build:
+# pyi-makespec --name="CAP Waldo Flightline Converter" --windowed --onefile --add-data="logo.png;." --add-data="flightline_utils.py;." vgui.py
+# pyi-makespec --name="CAP Waldo Flightline Converter" --windowed --onefile --add-data="logo.png;." --add-data="flightline_utils.py;." --hidden-import PyQt5 --hidden-import PyQt5.QtCore --hidden-import PyQt5.QtGui --hidden-import PyQt5.QtWidgets vgui.py
+
+# edit datas to have add         ('flightline_utils.py', '.'),  # Add this line
+# pyinstaller "CAP Waldo Flightline Converter.spec"
+
+class ConverterApp(QWidget):
+    def __init__(self, is_web=False):
+        super().__init__()
+        self.is_web = is_web
+        self.flightlines = []
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('CAP Waldo Flightline Converter')
+        self.setMinimumSize(600, 450)
+
+        main_layout = QVBoxLayout()
+
+        # Scroll area for the main content
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll_content = QWidget()
+        scroll_layout = QVBoxLayout(scroll_content)
+
+        # Logo
+        logo_label = QLabel(self)
+        logo_path = resource_path('logo.png')
+        pixmap = QPixmap(logo_path)  # Replace with your actual logo file
+        pixmap = pixmap.scaled(256, 256, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        logo_label.setPixmap(pixmap)
+        logo_label.setAlignment(Qt.AlignCenter)
+        scroll_layout.addWidget(logo_label)
+
+        # Title
+        title_label = QLabel('CAP Waldo Flightline Converter')
+        title_label.setAlignment(Qt.AlignCenter)
+        title_label.setStyleSheet("font-size: 20px; font-weight: bold;")
+        scroll_layout.addWidget(title_label)
+
+        # Input file selection
+        input_layout = QHBoxLayout()
+        self.input_line = QLineEdit()
+        self.input_line.setReadOnly(True)
+        input_layout.addWidget(self.input_line)
+        self.input_button = QPushButton('Choose File')
+        self.input_button.clicked.connect(self.select_input_file if not self.is_web else self.web_select_input_file)
+        input_layout.addWidget(self.input_button)
+        scroll_layout.addLayout(input_layout)
+
+        # Output file selection
+        output_layout = QHBoxLayout()
+        output_label = QLabel('Output path:')
+        output_layout.addWidget(output_label)
+        self.output_line = QLineEdit()
+        self.output_line.setReadOnly(True)
+        output_layout.addWidget(self.output_line)
+        self.output_button = QPushButton('Choose File')
+        self.output_button.clicked.connect(self.select_output_file if not self.is_web else self.web_set_output_file)
+        output_layout.addWidget(self.output_button)
+        scroll_layout.addLayout(output_layout)
+
+        # Preprocess button
+        self.preprocess_button = QPushButton('Preprocess')
+        self.preprocess_button.clicked.connect(self.preprocess)
+        scroll_layout.addWidget(self.preprocess_button)
+
+        # Flightline selection group
+        self.flightline_group = QGroupBox("Starting Corner Selection")
+        self.flightline_group.setVisible(False)
+        flightline_layout = QVBoxLayout()
+        self.corner_radio_1 = QRadioButton("First corner")
+        self.corner_radio_2 = QRadioButton("Second corner")
+        self.corner_radio_1.setChecked(True)
+        flightline_layout.addWidget(self.corner_radio_1)
+        flightline_layout.addWidget(self.corner_radio_2)
+        self.flightline_group.setLayout(flightline_layout)
+        scroll_layout.addWidget(self.flightline_group)
+
+        # Pattern selection group
+        self.pattern_group = QGroupBox("Flight Pattern")
+        self.pattern_group.setVisible(False)
+        pattern_layout = QHBoxLayout()
+        self.sawtooth_radio = QRadioButton("Sawtooth")
+        self.unidirectional_radio = QRadioButton("Unidirectional")
+        self.sawtooth_radio.setChecked(True)
+        self.sawtooth_radio.toggled.connect(self.update_keyhole_availability)
+        self.unidirectional_radio.toggled.connect(self.update_keyhole_availability)
+        pattern_layout.addWidget(self.sawtooth_radio)
+        pattern_layout.addWidget(self.unidirectional_radio)
+        self.pattern_group.setLayout(pattern_layout)
+        scroll_layout.addWidget(self.pattern_group)
+
+        # Options group
+        self.options_group = QGroupBox("Additional Options")
+        self.options_group.setVisible(False)
+        options_layout = QVBoxLayout()
+        self.extension_checkbox = QCheckBox("Extension points")
+        self.extension_checkbox.setChecked(True)
+        self.keyhole_checkbox = QCheckBox("Keyhole points")
+        self.keyhole_checkbox.setChecked(False)
+        options_layout.addWidget(self.extension_checkbox)
+        options_layout.addWidget(self.keyhole_checkbox)
+        self.options_group.setLayout(options_layout)
+        scroll_layout.addWidget(self.options_group)
+
+        # Export button
+        self.export_button = QPushButton('Export')
+        self.export_button.clicked.connect(self.export)
+        self.export_button.setVisible(False)
+        scroll_layout.addWidget(self.export_button)
+
+        scroll.setWidget(scroll_content)
+        main_layout.addWidget(scroll)
+
+        self.setLayout(main_layout)
+
+    def select_input_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Select KML File", "", "KML Files (*.kml)")
+        if file_name:
+            self.input_line.setText(file_name)
+            self.update_output_path(file_name)
+
+    def select_output_file(self):
+        file_name, _ = QFileDialog.getSaveFileName(self, "Save FPL File", "", "FPL Files (*.fpl)")
+        if file_name:
+            self.output_line.setText(file_name)
+
+    def web_select_input_file(self):
+        file_content = webview.windows[0].create_file_dialog(webview.OPEN_DIALOG, allow_multiple=False, file_types=['KML Files (*.kml)'])
+        if file_content:
+            self.process_kml_content(file_content[0])
+
+    def web_set_output_file(self):
+        filename, ok = QInputDialog.getText(self, 'Set Output Filename', 'Enter filename (without extension):')
+        if ok and filename:
+            self.output_line.setText(filename + '.fpl')
+
+    def update_output_path(self, input_path):
+        dir_name = os.path.dirname(input_path)
+        base_name = os.path.splitext(os.path.basename(input_path))[0]
+        output_path = os.path.join(dir_name, f"{base_name}.fpl")
+        self.output_line.setText(output_path)
+
+    def preprocess(self):
+        kml_file = self.input_line.text()
+        if not kml_file:
+            QMessageBox.warning(self, "Error", "Please select a KML file first.")
+            return
+
+        try:
+            tree = ET.parse(kml_file)
+            self.process_kml_tree(tree)
+        except ET.ParseError:
+            QMessageBox.warning(self, "Error", f"File {kml_file} is not a valid KML file. Please select a valid KML file.")
+
+    def process_kml_content(self, content):
+        try:
+            tree = ET.fromstring(content)
+            self.process_kml_tree(tree)
+        except ET.ParseError:
+            QMessageBox.warning(self, "Error", "Invalid KML file. Please select a valid KML file.")
+
+    def process_kml_tree(self, tree):
+        root = tree.getroot()
+        self.flightlines = []
+
+        for placemark in root.iter("{http://www.opengis.net/kml/2.2}Placemark"):
+            name = placemark.find("{http://www.opengis.net/kml/2.2}name").text
+            if name.startswith(" Flightline"):
+                coordinates_tag = placemark.find("{http://www.opengis.net/kml/2.2}LineString/{http://www.opengis.net/kml/2.2}coordinates")
+                if coordinates_tag is not None:
+                    coordinates = self.extract_coordinates(coordinates_tag)
+                    self.flightlines.append(coordinates)
+
+        if self.flightlines:
+            self.update_flightline_display()
+            self.flightline_group.setVisible(True)
+            self.pattern_group.setVisible(True)
+            self.options_group.setVisible(True)
+            self.export_button.setVisible(True)
+            QMessageBox.information(self, "Preprocess Complete", f"Preprocessing complete. Found {len(self.flightlines)} flightlines.")
+        else:
+            QMessageBox.warning(self, "Error", "No flightlines found in the KML file.")
+
+    def extract_coordinates(self, coordinates_tag):
+        coordinates = coordinates_tag.text.strip().split()
+        coordinates = [coord.split(',') for coord in coordinates]
+        #remember, two coordinates per row and lat/long flipped in kml.  Returning 1,0 instead of 0,1 fixes that for everything
+        return [(float(coord[1]), float(coord[0])) for coord in coordinates]
+
+    def update_flightline_display(self):
+        if self.flightlines:
+            self.corner_radio_1.setText(f"First corner: {self.flightlines[0][0]}")
+            self.corner_radio_2.setText(f"Second corner: {self.flightlines[0][1]}")
+
+    def update_keyhole_availability(self):
+        self.keyhole_checkbox.setEnabled(self.sawtooth_radio.isChecked())
+        if self.unidirectional_radio.isChecked():
+            self.keyhole_checkbox.setChecked(False)
+
+    def export(self):
+        if not self.flightlines:
+            QMessageBox.warning(self, "Error", "No flightlines to export. Please preprocess a KML file first.")
+            return
+
+        corner_choice = 1 if self.corner_radio_1.isChecked() else 2
+        flip = self.sawtooth_radio.isChecked()
+        extension_points = self.extension_checkbox.isChecked()
+        keyhole_points = self.keyhole_checkbox.isChecked()
+        
+        processed_flightlines = self.flightlines.copy()
+
+        if extension_points:
+            processed_flightlines = add_endpoints(processed_flightlines)
+
+        processed_flightlines = flip_rows(processed_flightlines, corner_choice, flip)
+
+        if keyhole_points:
+            processed_flightlines = add_keyholes(processed_flightlines)
+
+        output_file = self.output_line.text()
+        if not output_file:
+            QMessageBox.warning(self, "Error", "No output file specified. Please choose an output file.")
+            return
+
+        flightlines_output = output_file[:-4] + " flightlines.txt"
+
+        original_kml = self.input_line.text()
+        prefix = os.path.basename(original_kml)[:1]
+
+        try:
+            if self.is_web:
+                flightlines_content = io.StringIO()
+                output_points(flightlines_content, processed_flightlines)
+                flightlines_content.seek(0)
+
+                fpl_content = io.StringIO()
+                fpl_export(processed_flightlines, fpl_content, prefix)
+                fpl_content.seek(0)
+
+                webview.windows[0].evaluate_js(f"downloadFile('{os.path.basename(flightlines_output)}', '{flightlines_content.getvalue()}')")
+                webview.windows[0].evaluate_js(f"downloadFile('{os.path.basename(output_file)}', '{fpl_content.getvalue()}')")
+            else:
+                output_points(flightlines_output, processed_flightlines)
+                fpl_export(processed_flightlines, output_file, prefix)
+
+            QMessageBox.information(self, "Export Complete", 
+                                    f"Export completed successfully with the following settings:\n"
+                                    f"Starting corner: {'First' if corner_choice == 1 else 'Second'}\n"
+                                    f"Pattern: {'Sawtooth' if flip else 'Unidirectional'}\n"
+                                    f"Extension points: {'Added' if extension_points else 'Not added'}\n"
+                                    f"Keyhole points: {'Added' if keyhole_points else 'Not added'}\n"
+                                    f"Flightlines output: {flightlines_output}\n"
+                                    f"FPL output: {output_file}")
+        except Exception as e:
+            QMessageBox.critical(self, "Export Error", f"An error occurred while exporting:\n{str(e)}")
+
+def run_app():
+    app = QApplication(sys.argv)
+    ex = ConverterApp(is_web=False)
+    ex.show()
+    sys.exit(app.exec_())
+
+def run_web_app():
+    def expose_file_dialog(window):
+        window.expose(window.create_file_dialog)
+
+    webview.create_window("CAP Waldo Flightline Converter", ConverterApp(is_web=True), js_api=expose_file_dialog)
+    webview.start()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '--web':
+        run_web_app()
+    else:
+        run_app()
